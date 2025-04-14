111dc92740c5cb330f6ffeccf7be9009a2c455d6
Marcelo Regali <marcelo8924@hotmail.com>
2025-04-13 23:29:10 -0300

    Prueba3

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import mocksRouter from "./routes/mocks.router.js";
 import "reflect-metadata";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+//import usersRouter from "./routes/users.router.js";
+//import petsRouter from "./routes/pets.router.js";
+//import adoptionsRouter from "./routes/adoption.router.js";
+//import sessionsRouter from "./routes/sessions.router.js"; //
+
 // Middleware para recibir JSON
 app.use(express.json());
+app.use(cookieParser());
+
+//app.use("/api/users", usersRouter);
+//app.use("/api/pets", petsRouter);
+//app.use("/api/adoptions", adoptionsRouter);
+//app.use("/api/sessions", sessionsRouter);
 
 // Conectar a MongoDB
 mongoose.set("strictQuery", false); // Evita el warning de Mongoose
